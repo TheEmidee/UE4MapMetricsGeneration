aa8543c24c78fe6f6a577b88897be5a7fccc2e20
Dimitri Paccagnini <dimitri.dimitri@fishingcactus.com>
2024-11-04 12:06:14 +0100

    placeholder map

--- Source/Private/PerfGrapherCommandlet.cpp
+++ Source/Private/PerfGrapherCommandlet.cpp
@@ -126,7 +126,7 @@ bool UPerfGrapherCommandlet::ParseParams( const FString & params, FMetricsParams
     }
 
     // Set defaults before parsing overrides
-    out_params.MapName = TEXT( "L_OW" );
+    out_params.MapName = TEXT( "default_map" ); // :NOTE: This is a placeholder you can edit to set a default map
     out_params.CellSize = 1000.0f;
     out_params.GridOffset = FVector::ZeroVector;
     out_params.CameraHeight = 170.0f;
